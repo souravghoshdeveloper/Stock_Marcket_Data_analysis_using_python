c2cb1a06ae65b01e76715a31936288b5bf729915
Sourav Ghosh <infosourav04@gmail.com>
2021-07-10 12:08:26 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import quandl as qd
+import matplotlib.pyplot as matplt
+import numpy as np
+
+qd.ApiConfig.api_key = 'D7NyVWy1fazEisAuyhHX'
+
+Microsoft_data = qd.get('EOD/MSFT', start_date="2010-07-09", end_date="2021-07-09")
+Microsoft = Microsoft_data.head()
+# print(Microsoft)
+
+# Calculate the Returns
+close_price = Microsoft_data[['Adj_Close']]
+daily_return = close_price.pct_change()
+daily_return.fillna(0, inplace=True)
+# print(daily_return)
+
+adj_price = Microsoft_data[['Adj_Close']]
+
+# Calculate the moving average
+mav = adj_price.rolling(window=50).mean()
+
+# Print the results
+print(mav[-10:])
+
+adj_price.plot()
+mav.plot()
+matplt.show()
